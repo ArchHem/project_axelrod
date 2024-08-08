694f5ebab0fc8aa7f3f52c04b0b3ba7a6665c6de
ArchHem <84734676+ArchHem@users.noreply.github.com>
2024-08-08 17:07:25 +0200

    Split file

--- agent_based.jl
+++ agent_based.jl
@@ -135,27 +135,3 @@ end
 
 export TFT, random_picker, pavlov, clash_models!
 end
-
-using .AxelRod
-
-#example working 
-
-all_defector = random_picker(0.0)
-titfortat = TFT()
-pavlov_agent = pavlov()
-
-const p_corruption = 0.2
-score1, score2 = clash_models!(all_defector,titfortat,p_corrupt = p_corruption, N_turns = 100)
-score3, score4 = clash_models!(titfortat,pavlov_agent,p_corrupt = p_corruption, N_turns = 100)
-
-#for p = 0.0
-# we expect the all-defector/TFT matchup to end up with 1 cooperation/defection followed by all-defection: I.e. the defector should have 5 points more, 
-#and the TFT model should have N_turn-1 points. 
-# pavlov and TFT should cooperate till the end
-
-println(score1)
-println(score2)
-
-println(score3)
-println(score4)
-
---
+++ agent_tests.jl
@@ -0,0 +1,23 @@
+include("./agent_based.jl")
+
+using Random, .AxelRod
+
+
+all_defector = random_picker(0.0)
+titfortat = TFT()
+pavlov_agent = pavlov()
+
+const p_corruption = 0.1
+score1, score2 = clash_models!(all_defector,titfortat,p_corrupt = p_corruption, N_turns = 100)
+score3, score4 = clash_models!(titfortat,pavlov_agent,p_corrupt = p_corruption, N_turns = 100)
+
+#for p = 0.0
+# we expect the all-defector/TFT matchup to end up with 1 cooperation/defection followed by all-defection: I.e. the defector should have 5 points more, 
+#and the TFT model should have N_turn-1 points. 
+# pavlov and TFT should cooperate till the end
+
+println(score1)
+println(score2)
+
+println(score3)
+println(score4)
